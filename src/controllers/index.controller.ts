d9e4474f7bbb952e5c70bd2fa52c9062043cf38d
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-11-08 11:44:14 +0530

    Update index.controller.ts

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,3 +1,5 @@
+import { AWS_BUCKET_NAME } from '@/config';
+import AWS from 'aws-sdk';
 import { NextFunction, Request, Response } from 'express';
 
 export const home = (req: Request, res: Response, next: NextFunction) => {
@@ -10,3 +12,25 @@ export const home = (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 };
+
+
+export const readstream = async (req: Request, res: Response) => {
+
+  const file_key= req.params.key
+  const s3 = new AWS.S3();
+  const bucketName = AWS_BUCKET_NAME; // Replace with your S3 bucket name
+  const objectKey = `uploads/${file_key}`
+
+  const params = {
+      Bucket: bucketName,
+      Key: objectKey,
+  };
+try{
+  const fileStream = s3.getObject(params).createReadStream();
+  
+   fileStream.pipe(res);
+}catch(error){
+  res.json("key not found")
+}
+}
+
